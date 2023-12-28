04ef76741e41dbba46a83f651fe73d2992035434
tareshdhamir <taresh.dhamir@gmail.com>
2023-12-29 03:52:35 +0530

    Implement tabs for auth-modal

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -6,33 +6,33 @@ interface IModal{
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ModalService {
-
-  private visible = false;
   private modals: IModal[] = [];
 
-  constructor() { 
+  constructor() {}
+
+  unregister(id: string) {
+    this.modals = this.modals.filter(element => element.id !== id);
   }
 
-  register(id:string){
+  register(id: string) {
     this.modals.push({
       id,
-      visible: false
+      visible: false,
     });
   }
 
-  isModalOpen(id: string): boolean{
-    return !!this.modals.find(element => element.id === id)?.visible;
+  isModalOpen(id: string): boolean {
+    return !!this.modals.find((element) => element.id === id)?.visible;
   }
 
-  toggleModal(id: string){
-    const modal = this.modals.find(element => element.id === id);
+  toggleModal(id: string) {
+    const modal = this.modals.find((element) => element.id === id);
 
-    if(modal){
+    if (modal) {
       modal.visible = !modal.visible;
     }
   }
-
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -11,11 +11,11 @@ export class ModalComponent implements OnInit{
   @Input()
   modalId: string = '';
 
-  constructor(public modal: ModalService){
+  constructor(public modal: ModalService, public elementRef: ElementRef){
   }
 
   ngOnInit(): void{
-
+    document.body.appendChild(this.elementRef.nativeElement);
   }
   closeModal(){
     this.modal.toggleModal(this.modalId);
