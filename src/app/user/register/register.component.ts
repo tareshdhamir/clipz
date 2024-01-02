16ea282b7c0d7e81955e413111b3b2319e448bba
tareshdhamir <taresh.dhamir@gmail.com>
2024-01-03 02:11:46 +0530

    Set-up Firebase

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,37 +1,52 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  inSubmission = false;
 
-    name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-    email = new FormControl('', [Validators.required, Validators.email]);
-    age = new FormControl('',[Validators.required, Validators.min(18), Validators.max(120)]);
-    password = new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)]);
-    confirm_password = new FormControl('', [Validators.required]);
-    phoneNumber = new FormControl('', [Validators.required, Validators.minLength(13), Validators.maxLength(13)]);
+  constructor(private auth: AngularFireAuth) {}
+
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+  ]);
+  confirm_password = new FormControl('', [Validators.required]);
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13),
+  ]);
 
   registerForm = new FormGroup({
     name: this.name,
     email: this.email,
-    passowrd: this.password,
+    password: this.password,
     confirm_passowrd: this.confirm_password,
     age: this.age,
-    phoneNumber: this.phoneNumber
+    phoneNumber: this.phoneNumber,
   });
 
   showAlert = false;
   alertMsg = 'Please wait! Your account is being created';
   alertColor = 'blue';
 
-  register(){
+  async register() {
+    this.inSubmission = true;
     this.showAlert = true;
     this.alertMsg = 'Please wait! Your account is being created';
     this.alertColor = 'blue';
   }
-
 }
