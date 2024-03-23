2fdcddb63a3396b9c1f0ba94161ff652c46279c9
tareshdhamir <taresh.dhamir@gmail.com>
2024-03-24 03:30:55 +0530

    Configure app for deployment to firebase hosting

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyDdmEkK0v_6U_MpIGxr5FM4TihzZV7904o",
-        authDomain: "clipz-4e0ec.firebaseapp.com",
+        apiKey: "", //place firebase apiKey here
+        authDomain: "clipz-4e0ec.firebaseapp.com", //replace 'clipz-4e0ec' with name of your hosted application if applicable
         projectId: "clipz-4e0ec",
         storageBucket: "clipz-4e0ec.appspot.com",
-        appId: "1:309594397360:web:088b5d42ea527d7aed7975"
+        appId: "" //place firebase appId here
       }
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-    production: true,
-    firebase: {
-        apiKey: "AIzaSyDdmEkK0v_6U_MpIGxr5FM4TihzZV7904o",
-        authDomain: "clipz-4e0ec.firebaseapp.com",
-        projectId: "clipz-4e0ec",
-        storageBucket: "clipz-4e0ec.appspot.com",
-        appId: "1:309594397360:web:088b5d42ea527d7aed7975"
-      }
+  production: false,
+  firebase: {
+      apiKey: "", //place firebase apiKey here
+      authDomain: "clipz-4e0ec.firebaseapp.com", //replace 'clipz-4e0ec' with name of your hosted application if applicable
+      projectId: "clipz-4e0ec",
+      storageBucket: "clipz-4e0ec.appspot.com",
+      appId: "" //place firebase appId here
+    }
 };
 
